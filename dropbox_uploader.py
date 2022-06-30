df7d5d7e10d63268a22f760f7fa66b0198743ff9
Marcin Popko <marcinpopko@outlook.com>
2022-06-30 12:04:02 +0200

    Add files via upload
    Try except within dropbox upload.

--- dropbox_uploader.py
+++ dropbox_uploader.py
@@ -18,25 +18,28 @@ class backuper:
         self.BACKUPPATH = '/TitlesDatabase/titles_' + currentTime + '.txt'
     # Uploads contents of LOCALFILE to Dropbox
     def backup(self):
-        currentTime = datetime.now().strftime('%Y-%m-%d')
-        with open(self.LOCALFILE, 'rb') as f:
-            # We use WriteMode=overwrite to make sure that the settings in the file
-            # are changed on upload
-            print("Uploading " + self.LOCALFILE + " to Dropbox as " + self.BACKUPPATH + "...")
-            self.logger.info(f"Uploading {self.LOCALFILE} to Dropbox as {self.BACKUPPATH}...")
-            try:
-                dbx.files_upload(f.read(), self.BACKUPPATH, mode=WriteMode('overwrite'))
-            except ApiError as err:
-                # This checks for the specific error where a user doesn't have
-                # enough Dropbox space quota to upload this file
-                if (err.error.is_path() and
-                        err.error.get_path().reason.is_insufficient_space()):
-                    self.logger.error(f"ERROR: Cannot back up; insufficient space.")
-                    sys.exit("ERROR: Cannot back up; insufficient space.")
-                elif err.user_message_text:
-                    print(err.user_message_text)
-                    self.logger.error(f"ERROR: {err.user_message_text}")
-                    sys.exit()
-                else:
-                    print(err)
-                    sys.exit()
+        try:
+            currentTime = datetime.now().strftime('%Y-%m-%d')
+            with open(self.LOCALFILE, 'rb') as f:
+                # We use WriteMode=overwrite to make sure that the settings in the file
+                # are changed on upload
+                print("Uploading " + self.LOCALFILE + " to Dropbox as " + self.BACKUPPATH + "...")
+                self.logger.info(f"Uploading {self.LOCALFILE} to Dropbox as {self.BACKUPPATH}...")
+                try:
+                    dbx.files_upload(f.read(), self.BACKUPPATH, mode=WriteMode('overwrite'))
+                except ApiError as err:
+                    # This checks for the specific error where a user doesn't have
+                    # enough Dropbox space quota to upload this file
+                    if (err.error.is_path() and
+                            err.error.get_path().reason.is_insufficient_space()):
+                        self.logger.error(f"ERROR: Cannot back up; insufficient space.")
+                        sys.exit("ERROR: Cannot back up; insufficient space.")
+                    elif err.user_message_text:
+                        print(err.user_message_text)
+                        self.logger.error(f"ERROR: {err.user_message_text}")
+                        sys.exit()
+                    else:
+                        print(err)
+                        sys.exit()
+        except:
+            self.logger.error(f"Upload to Dropbox error.")
